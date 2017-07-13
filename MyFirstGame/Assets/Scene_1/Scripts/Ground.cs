005edff1c424a50be602a8ee6ed89cec7a06cd08
ilaydacansinkoc <ilaydacansin@gmail.com>
2017-07-14 00:36:42 +0300

    First Commit
    Added Unity Project to GitHub (In Progress)

---
+++ MyFirstGame/Assets/Scene_1/Scripts/Ground.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ground : MonoBehaviour {
+
+
+    private Player player;
+
+	// Use this for initialization
+	void Start () {
+
+        player = gameObject.GetComponentInParent<Player>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        player.grounded = true;
+
+    }
+    void OnTriggerStay2D(Collider2D collider)
+    {
+        player.grounded = true;
+
+    }
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        player.grounded = false;
+    }
+}
---
+++ MyFirstGame/Assets/Scene_1/Scripts/Player.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public GameObject obj;
+    private Rigidbody2D rb2d;
+    private Animator anim;
+
+    [SerializeField]
+    private float movSpeed;
+
+    public float jumpPower = 100f;
+    private bool canDoubleJump=false;
+    public bool grounded;
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("I am alive.");
+        anim = gameObject.GetComponent<Animator>();
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        anim.SetBool("grounded", grounded);
+
+        float horizontal = Input.GetAxis("Horizontal");
+        handleMovement(horizontal);
+
+                 
+    }
+
+    private void handleMovement(float horizontal)
+    {
+
+        rb2d.velocity = new Vector2(horizontal * movSpeed, rb2d.velocity.y);
+
+        if(Input.GetButtonDown("Jump")){
+
+            if (grounded)
+            {
+                rb2d.AddForce(Vector2.up * jumpPower);
+                canDoubleJump = true;
+            }
+
+            else
+            {
+                if (canDoubleJump)
+                {
+                    canDoubleJump = false;
+                    rb2d.velocity = new Vector2(rb2d.velocity.x, 0);
+                    rb2d.AddForce(Vector2.up * jumpPower);
+                }
+            }
+        }
+
+    }
+}
