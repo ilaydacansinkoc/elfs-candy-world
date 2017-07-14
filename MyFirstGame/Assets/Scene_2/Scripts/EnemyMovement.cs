11cf7346bc135e4e26e9986c7c0c23d1d406bdf9
ilaydacansinkoc <ilaydacansin@gmail.com>
2017-07-14 22:03:21 +0300

    Second Scene
    In Progress

---
+++ MyFirstGame/Assets/Scene_2/Scripts/EnemyMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+
+    public GameObject obj;
+    static float t = 0.0f;
+    private float minimum = -1.5f;
+    private float maximum = 2.5f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        obj.transform.position = new Vector3(obj.transform.position.x, Mathf.Lerp(minimum,maximum,t),obj.transform.position.z);
+        t += 0.5f * Time.deltaTime;
+
+        // now check if the interpolator has reached 1.0
+        // and swap maximum and minimum so game object moves
+        // in the opposite direction.
+        if (t > 1.0f)
+        {
+            float temp = maximum;
+            maximum = minimum;
+            minimum = temp;
+            t = 0.0f;
+        }
+    }
+}
