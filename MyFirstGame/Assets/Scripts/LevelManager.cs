9ec7254210aee077eb85ff677c74d9caf455f6bb
ilaydacansinkoc <ilaydacansin@gmail.com>
2017-07-24 18:58:58 +0300

    Game Menu Added

---
+++ MyFirstGame/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+    
+    public void playButton(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+
+    public void exitButton()
+    {
+        Application.Quit();
+    }
+
+    public void optionsButton()
+    {
+
+    }
+}
--- MyFirstGame/Assets/Scripts/Player.cs
+++ MyFirstGame/Assets/Scripts/Player.cs
@@ -17,6 +17,8 @@ public class Player : MonoBehaviour {
     private AudioSource source_1;
     private AudioSource source_2;
     private AudioSource source_3;
+    private AudioSource source_4;
+    private AudioSource source_5;
     private AudioSource[] audioSources;
 
 
@@ -34,6 +36,8 @@ public class Player : MonoBehaviour {
         source_1 = audioSources[0];
         source_2 = audioSources[1];
         source_3 = audioSources[2];
+        source_4 = audioSources[3];
+        source_5 = audioSources[4];
 
 	}
 	
@@ -98,6 +102,17 @@ public class Player : MonoBehaviour {
         if(collision.gameObject.tag == "door_2")
         {
             SceneManager.LoadScene("scene_3");
+        }
+
+        if(collision.gameObject.tag == "bottom_collider")
+        {
+            source_4.Play();
+        }
+
+        if(collision.gameObject.tag == "lava")
+        {
+            source_5.Play();
+            source_4.enabled = false;
         }
     }
 
